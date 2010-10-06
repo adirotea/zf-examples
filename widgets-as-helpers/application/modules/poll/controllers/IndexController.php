5276bbdb7874133f38478e0f70aebe2a0f6daec7
adirotea <adrian.rotea@gmail.com>
2010-10-07 01:22:21 +0300

    Appended custom text to response body from poll index controller to reflect that is being executted

--- widgets-as-helpers/application/modules/poll/controllers/IndexController.php
+++ widgets-as-helpers/application/modules/poll/controllers/IndexController.php
@@ -11,6 +11,7 @@ class Poll_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $this->getResponse()->appendBody('I come from Poll');
     }
 
 	public function createAction()
